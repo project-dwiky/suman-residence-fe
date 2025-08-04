4a3532b49610f386e52aef33d449f930c7cb399e
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-08-04 22:18:19 +0700

    test redeploy

--- src/app/admin/dashboard/booking/page.tsx
+++ src/app/admin/dashboard/booking/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import BookingManagement from "@/components/admin/BookingManagement";
+// test redeploy
 
 export default function BookingManagementPage() {
   return <BookingManagement />;
